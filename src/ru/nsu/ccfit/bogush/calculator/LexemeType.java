9ecd3088148d43819ff7c74424488058312d5168
RoyPy <bogush.vano@gmail.com>
2018-02-06 12:34:47 +0700

    Implement Lexer

---
+++ src/ru/nsu/ccfit/bogush/calculator/LexemeType.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.bogush.calculator;
+
+public enum LexemeType {
+    PLUS,
+    MINUS,
+    OPEN_BRACKET,
+    CLOSE_BRACKET,
+    MULT,
+    DIV,
+    POWER,
+    EOF,
+    NUMBER;
+}
